c43f6434a8ff3f14cdbbfc45dacda18a8d3aad69
oleg <oleg.pav.m@gmail.com>
2023-11-04 22:45:59 +0200

    Fix pennant profit.

--- apps/bot/src/analyzer/detector/detect/pennant.detect.ts
+++ apps/bot/src/analyzer/detector/detect/pennant.detect.ts
@@ -4,7 +4,7 @@ import { DetectorService } from '../detector.service';
 import { EHmaType } from '../../../data/candle.model';
 
 export class PennantDetect extends AbstractDetect {
-    protected profitMul = 2.75;
+    protected profitMul = 2.65;
     protected enterFib = 0.85;
     protected takeFib = 2.3;
     protected stopFib = 0.62;
