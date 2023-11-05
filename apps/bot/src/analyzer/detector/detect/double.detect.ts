b2bdea2e302f74e1419706fb459cba410eb9fbe7
oleg <oleg.pav.m@gmail.com>
2023-11-05 23:00:02 +0200

    Double detect.

--- apps/bot/src/analyzer/detector/detect/double.detect.ts
+++ apps/bot/src/analyzer/detector/detect/double.detect.ts
@@ -4,21 +4,47 @@ import { DetectorService } from '../detector.service';
 import { EHmaType } from '../../../data/candle.model';
 
 export class DoubleDetect extends AbstractDetect {
-    protected profitMul = 1.95;
+    protected profitMul = 2.3;
     protected enterFib = 0.85;
     protected takeFib = 2;
     protected stopFib = 0.62;
 
+    protected minSegmentSize = 2;
+    protected maxSecondSegmentSize = 2;
     protected waitDays = 2;
 
     check(): boolean {
-        const [down0, up1, down1, up2, down2] = this.getWaves(5, false);
+        const [down0, up1, down1, up2, down2, up3] = this.getWaves(6, false);
 
         if (!down2) {
             return;
         }
 
-        // TODO -
+        const notOverflow = up1.maxGte(down0.max);
+        const trendOffsetFirst = this.getFib(up2.max, down2.min, 0.5, true);
+        const pennantBodyOffsetSecond = this.getFib(up1.max, down0.min, 0.5, true);
+        const candleInHalfDownSecond = down0.candles.some((candle) =>
+            this.lt(this.candleMax(candle), pennantBodyOffsetSecond),
+        );
+
+        if (
+            notOverflow &&
+            down0.sizeLeft >= this.minSegmentSize &&
+            down1.minGt(trendOffsetFirst) &&
+            up3.maxLt(down0.min) &&
+            candleInHalfDownSecond &&
+            up1.maxGt(up2.max) &&
+            down0.minGt(down1.min) &&
+            down0.minLt(up2.max)
+        ) {
+            if (this.isCurrentSegmentDown() || down0.sizeRight < this.maxSecondSegmentSize) {
+                return this.markDetection();
+            } else {
+                return this.markEndDetection();
+            }
+        } else {
+            return this.markEndDetection();
+        }
     }
 }
 
