6dc45be904bb387a0ec55b19d93362eca4421eda
oleg <opa@exante.eu>
2024-01-18 21:34:46 +0200

    Remove bot logic.

--- apps/bot/src/bot.module.ts
+++ apps/bot/src/bot.module.ts
@@ -3,23 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CandleModel } from './data/candle.model';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AnalyzerModule } from './analyzer/analyzer.module';
-import { AdminModule } from './admin/admin.module';
-import { TraderModule } from './trader/trader.module';
-import { BotService } from './bot.service';
-import { BotModel } from './data/bot.model';
-import { TelegrafModule } from 'nestjs-telegraf';
-import { BotLogModel } from './data/bot-log.model';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
-        }),
-        TelegrafModule.forRootAsync({
-            useFactory: async (configService: ConfigService) => ({
-                token: configService.get<string>('F_TG_KEY'),
-            }),
-            inject: [ConfigService],
         }),
         TypeOrmModule.forRootAsync({
             inject: [ConfigService],
@@ -27,15 +15,13 @@ import { BotLogModel } from './data/bot-log.model';
                 type: 'sqlite',
                 database: 'base.db',
                 key: configService.get('F_DB_KEY'),
-                entities: [CandleModel, BotModel, BotLogModel],
+                entities: [CandleModel],
                 synchronize: true,
             }),
         }),
         AnalyzerModule,
-        AdminModule,
-        TraderModule,
     ],
     controllers: [],
-    providers: [BotService],
+    providers: [],
 })
 export class BotModule {}
--- apps/bot/src/main.ts
+++ apps/bot/src/main.ts
@@ -2,14 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { BotModule } from './bot.module';
 import { LoaderService } from './analyzer/loader/loader.service';
 import { CalculatorService } from './analyzer/calculator/calculator.service';
-import { BotService } from './bot.service';
 import { endOfYear, startOfYear } from './utils/time.util';
 
 async function bootstrap() {
     const app = await NestFactory.createApplicationContext(BotModule);
-
-    // Init service
-    app.get(BotService);
 
     await app.get(LoaderService).loadActual();
     await app.get(CalculatorService).calc({
