d39be07e0ec457809ad695242c194dadf6a3e382
oleg <opa@exante.eu>
2024-07-06 12:57:18 +0300

    Improves.

--- apps/bot/src/bot.types.ts
+++ apps/bot/src/bot.types.ts
@@ -16,7 +16,9 @@ export enum ETicker {
     AAPL = 'AAPL',
     EBAY = 'EBAY',
     UAL = 'UAL',
+    EURUSD = 'C:EURUSD',
+    MDB = 'MDB',
 }
 
 export const BINANCE_TICKERS = [ETicker.BTCUSDT];
-export const NEW_YORK_TICKERS = [ETicker.AAPL, ETicker.EBAY, ETicker.UAL];
+export const NEW_YORK_TICKERS = [ETicker.AAPL, ETicker.EBAY, ETicker.UAL, ETicker.EURUSD, ETicker.MDB];
